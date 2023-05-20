7edd3df52b234dc2f73081085467bc03de9b6456
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-05-20 15:02:17 +0200

    value-at-risk.R

---
+++ Statystyka i ekonometria/value_at_risk.R
@@ -0,0 +1,69 @@
+if(!require(pacman)) install.packages('pacman')
+pacman::p_load(rugarch, MASS, ggplot2)
+
+dax = datasets::EuStockMarkets[, 1]
+returns = diff(log(dax))
+
+alpha = 0.05
+
+# Historical method ----
+VaR_historical = quantile(returns, alpha, names = FALSE)
+
+# Parametric method (normal distribution) ----
+mu = mean(returns)
+sigma = sd(returns)
+VaR_parametric_normal = qnorm(alpha, mu, sigma)
+
+# Parametric method (t-Student distribution) ----
+normalized_returns = (returns - mu) / sigma
+fit = fitdistr(normalized_returns, 't')
+df = fit$estimate['df']
+VaR_parametric_t = mu + sigma * qt(alpha, df)
+
+# Simulation method (t-Student distribution) ----
+set.seed(2137)
+returns_simulated_t = mu + sigma * rt(10000, df)
+VaR_simulated_t = quantile(returns_simulated_t, alpha, names = FALSE)
+  
+# EWMA model ----
+lambda = 0.94
+var_ewma = rep(0, length(returns))
+
+for (i in seq_along(returns)) {
+  if (i == 1) {
+    var_ewma[i] = var(returns)
+  } else {
+    var_ewma[i] = lambda * var_ewma[i-1] + (1 - lambda) * returns[i-1]^2
+  }
+}
+
+VaR_EWMA = qnorm(alpha) * tail(sqrt(var_ewma), 1)
+
+# GARCH model ----
+spec = ugarchspec(
+  variance.model = list(model = 'sGARCH', garchOrder = c(1, 1))
+  , mean.model = list(armaOrder = c(0, 0), include.mean = TRUE)
+  , distribution.model = 'norm'
+)
+
+fit = ugarchfit(spec, returns)
+VaR_GARCH = qnorm(alpha) * tail(as.numeric(sigma(fit)), 1)
+
+# VaR vs different alpha levels ----
+alpha_levels = seq(0.001, 0.1, length.out = 1000)
+VaR_historical = sapply(alpha_levels, function(p) quantile(returns, p, names = FALSE))
+VaR_parametric_normal = sapply(alpha_levels, function(p) qnorm(p, mu, sigma))
+VaR_parametric_t = sapply(alpha_levels, function(p) mu + sigma * qt(p, df))
+VaR_EWMA = sapply(alpha_levels, function(p) qnorm(p) * tail(sqrt(var_ewma), 1))
+VaR_GARCH = sapply(alpha_levels, function(p) qnorm(p) * tail(as.numeric(sigma(fit)), 1))
+
+plot_data = data.frame(
+  Alpha = rep(alpha_levels, 5)
+  ,VaR = c(VaR_historical, VaR_parametric_normal, VaR_parametric_t, VaR_EWMA, VaR_GARCH)
+  ,Method = factor(rep(c('Historical', 'Parametrical (normal)', 'Parametrical (t)', 'EWMA', 'GARCH'), each = length(alpha_levels)))
+)
+
+ggplot(plot_data, aes(x = Alpha, y = VaR, color = Method)) +
+  geom_line(size = 0.75) +
+  labs(x = 'Alpha level', y = 'VaR', color = 'Method') +
+  theme(legend.position = 'top')
