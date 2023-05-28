c398c15a371d3a03112459326db7092b96b619ca
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-05-28 08:34:02 +0200

    Dokończenie skryptu o modelach linearyzowalnych

--- Statystyka i ekonometria/modele_linearyzowalne.R
+++ Statystyka i ekonometria/modele_linearyzowalne.R
@@ -1,8 +1,7 @@
 if(!require('pacman')) install.packages('pacman')
 pacman::p_load(purrr, dplyr, ggplot2, lmtest)
-theme_set(theme_bw())
-data(mtcars)
 
+# Define regression formulas
 formulas = list(
   'Linear' = formula('y ~ x')
   ,'Power' = formula('log(y) ~ log(x)')
@@ -18,8 +17,10 @@ formulas = list(
 # Fit models
 models = map(formulas, ~ lm(.x, data = mtcars %>% mutate(x = disp, y = mpg)))
 
+# Set significance level for model diagnostics
 alpha = 0.05
 
+# Model diagnostics
 model_tests = list(
   # Get R^2
   `R Squared` = map_dbl(map(models, summary), 'r.squared')
@@ -34,13 +35,29 @@ model_tests = list(
   ,`Homoscedastic residuals` = map_lgl(models, ~ bptest(.x)[['p.value']] > alpha)
   
   # Test for residuals independence
-  ,`Independent residuals` = map_lgl(models, ~ bptest(.x)[['p.value']] > alpha)
+  ,`Independent residuals` = map_lgl(models, ~ dwtest(.x)[['p.value']] > alpha)
 )
 
 model_tests_df = as_tibble(model_tests) %>%
   mutate(Model = names(formulas), .before = 1)
 
-
+# Plot R^2
 ggplot(model_tests_df, aes(y = reorder(Model, `R Squared`), x = `R Squared`)) +
-  geom_col(fill = 'steelblue')
-# Plot models
+  geom_col(fill = '#00BFC4') +
+  theme_bw() +
+  labs(y = '')
+
+# Plot models which satisfies linear regression assumptions
+ggplot(mtcars, aes(x = disp, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = 'lm', formula = 'y ~ log(x)', se = FALSE, aes(color = 'Log model')) +
+  geom_smooth(method = 'lm', formula = 'y ~ exp(I(1/x))', se = FALSE, aes(color = 'S-type model'))+
+  scale_color_manual(values = c("#00BFC4", "#7CAE00")) +
+  theme_bw() +
+  theme(
+    legend.position = c(0.8, 0.8)
+    , legend.background = element_blank()
+    , legend.title = element_text(size = 15)
+    , legend.text = element_text(size = 12)
+  ) +
+  labs(color = 'Model type')
