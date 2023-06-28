6e51b7ffa36efd9f6ece5e2da01971c1991dfdc4
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-06-28 21:05:40 +0200

    Value-at-risk for different time horizons

---
+++ Statystyka i ekonometria/var_time_horizons.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+options(scipen = 20)
+theme_set(theme_bw())
+
+# Get data from stooq ----
+df_daily = read.csv(url('https://stooq.pl/q/d/l/?s=dnp&i=d'), stringsAsFactors = FALSE) %>%
+  mutate(Data = as.Date(Data)) %>%
+  mutate(Returns = c(NA, diff(log(Zamkniecie)))) %>%
+  mutate(Frequency = 'Daily') %>%
+  na.omit()
+
+df_weekly = read.csv(url('https://stooq.pl/q/d/l/?s=dnp&i=w'), stringsAsFactors = FALSE) %>%
+  mutate(Data = as.Date(Data)) %>%
+  mutate(Returns = c(NA, diff(log(Zamkniecie)))) %>%
+  mutate(Frequency = 'Weekly') %>%
+  na.omit()
+
+# Volatility vs data frequency ----
+bind_rows(df_daily, df_weekly) %>%
+  ggplot(aes(x = Data, y = Returns, color = Frequency)) +
+  geom_line() +
+  labs(
+    title = 'Returns over time'
+    , x = 'Date'
+    , color = 'Frequency'
+  ) +
+  theme(legend.position = c(0.2, 0.85), legend.background = element_blank())
+
+sd(df_daily$Returns)
+sd(df_weekly$Returns)
+
+var.test(
+  df_weekly$Returns
+  , df_daily$Returns
+  , ratio = 1
+  , alternative = 'greater'
+)
+
+# Plot densities ----
+source('https://raw.githubusercontent.com/pawel-wieczynski/useful-functions/main/histogram_with_densities.R')
+histogram_with_densities(df_daily, 'Returns')
+histogram_with_densities(df_weekly, 'Returns')
+
+# Fit normal distribution ----
+m_daily = mean(df_daily$Returns)
+sd_daily = sd(df_daily$Returns)
+
+m_weekly = mean(df_weekly$Returns)
+sd_weekly = sd(df_weekly$Returns)
+
+# Calculate VaR ----
+alpha = 0.05
+
+VaR_daily = qnorm(alpha, m_daily, sd_daily)
+VaR_weekly = qnorm(alpha, m_weekly, sd_weekly)
+
+H = 5 # trading week has 5 days
+VaR_daily_H = VaR_daily * sqrt(H)
+
+# Monte Carlo ----
+N = 1e4
+simulated_weekly_returns = c()
+
+set.seed(2137)
+for (i in 1:N) {
+  returns = rnorm(H, m_daily, sd_daily)
+  cum_return = sum(returns) # log-returns are additive
+  simulated_weekly_returns = c(simulated_weekly_returns, cum_return)
+}
+
+VaR_MC = quantile(simulated_weekly_returns, alpha)
+
+# Plot results
+ggplot(df_weekly, aes(Returns)) + 
+  geom_histogram(fill = 'steelblue', color = 'white', bins = 50) +
+  geom_vline(aes(color = 'VaR from weekly returns', xintercept = VaR_weekly), size = 1) +
+  geom_vline(aes(color = 'Square root of time', xintercept = VaR_daily_H), size = 1) +
+  geom_vline(aes(color = 'Monte Carlo', xintercept = VaR_MC), size = 1) +
+  theme(legend.position = c(0.8, 0.8), legend.background = element_blank()) +
+  labs(color = 'Method', y = '', title = 'Weekly 5% Value-at-Risk')
