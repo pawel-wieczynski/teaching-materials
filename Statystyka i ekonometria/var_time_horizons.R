38e4504f1b735ef9b5e5f622fa924e44fd40e2a4
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-07-02 16:46:20 +0200

    Value-at-Risk for h = 1, ..., 252

--- Statystyka i ekonometria/var_time_horizons.R
+++ Statystyka i ekonometria/var_time_horizons.R
@@ -22,6 +22,7 @@ bind_rows(df_daily, df_weekly) %>%
   labs(
     title = 'Returns over time'
     , x = 'Date'
+    , y = 'Returns'
     , color = 'Frequency'
   ) +
   theme(legend.position = c(0.2, 0.85), legend.background = element_blank())
@@ -70,7 +71,7 @@ for (i in 1:N) {
 
 VaR_MC = quantile(simulated_weekly_returns, alpha)
 
-# Plot results
+# Plot results ----
 ggplot(df_weekly, aes(Returns)) + 
   geom_histogram(fill = 'steelblue', color = 'white', bins = 30) +
   geom_vline(aes(color = 'VaR from weekly returns', xintercept = VaR_weekly), size = 1) +
@@ -78,3 +79,37 @@ ggplot(df_weekly, aes(Returns)) +
   geom_vline(aes(color = 'Monte Carlo', xintercept = VaR_MC), size = 1) +
   theme(legend.position = c(0.8, 0.8), legend.background = element_blank()) +
   labs(color = 'Method', y = '', title = 'Weekly 5% Value-at-Risk')
+
+# H = 1, ... 252 ----
+H = 1:252
+VaR_year = VaR_daily * sqrt(H)
+
+VaR_year_MC = c()
+
+set.seed(2137)
+for (h in H) {
+  simulated_returns = c()
+  for (i in 1:N) {
+    returns = rnorm(h, m_daily, sd_daily)
+    cum_return = sum(returns)
+    simulated_returns = c(simulated_returns, cum_return)
+  }
+  VaR_year_MC[h] = quantile(simulated_returns, alpha)
+}
+
+VaR_year_df = tibble(
+  H = H
+  , 'Pierwiastek kwadratowy z czasu' = -100 * VaR_year
+  , 'Symulacje Monte Carlo' = -100 * VaR_year_MC
+)
+
+VaR_year_df %>%
+  pivot_longer(
+    cols = 2:3
+    , names_to = 'Metoda'
+    , values_to = 'Value-at-Risk'
+  ) %>%
+  ggplot(aes(x = H, y = `Value-at-Risk`)) +
+  geom_line(aes(color = Metoda), size = 1) +
+  theme(legend.position = c(0.2, 0.8), legend.background = element_blank()) +
+  labs(title = '5% Value-at-Risk dla różnych horyzontów czasowych')
