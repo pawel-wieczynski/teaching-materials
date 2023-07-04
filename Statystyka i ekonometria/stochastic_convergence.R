d55f6ba81da0b468473a753f08d4d6a9c8bb39db
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-07-04 19:51:57 +0200

    add convergence in probability

--- Statystyka i ekonometria/stochastic_convergence.R
+++ Statystyka i ekonometria/stochastic_convergence.R
@@ -1,9 +1,7 @@
-pacman::p_load(purrr, ggplot2, dplyr)
+pacman::p_load(purrr)
 
 # Convergence in distribution ----
 # Tossing a coin, let Xn be the fraction of heads after n tosses
-set.seed(2137)
-
 # X1 has Bernoulli-like distribution, it can have either 0% or 100% of heads
 # simulate X1 k = 1e4 times
 tosses = map_chr(1:1e4, ~ sample(c('H', 'T'), size = 1))
@@ -22,3 +20,12 @@ hist(X4)
 tosses = map(1:1e4, ~ sample(c('H', 'T'), size = 1e4, replace = TRUE))
 X1e4 = map_dbl(tosses, ~ sum(.x == 'H') / length(.x))
 hist(X1e4)
+
+# Convergence in probability ----
+# Tossing a coin and tracking the running proportion of heads
+# It should convergence to 0.5 as n increases
+# denote 0 = tails, 1 = heads
+tosses = map_dbl(1:1e4, ~ sample(c(0, 1), size = 1))
+plot(1:1e4, cumsum(tosses)/(1:1e4), type = 'l', ylim = c(0,1))
+abline(h = 0.5, col = "red", lwd = 1)
+
